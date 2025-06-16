088a6703ea7263a7bf5f586ee57cdb6f89d2f304
psypad <psypad20@gmail.com>
2025-06-16 11:23:00 +0530

    improved regex to check for /, >, <, | and add it to the token array

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -10,26 +10,18 @@ fn trim_newline(s: &mut String) {
     }
 }
 
-// TODO add the pipes in the tokenized array
-
 pub fn tokenizer(mut input: String){
 
     trim_newline(&mut input);
     println!("DEBUG (input string from tokenizer): {:?}", input);
 
-
     let mut arg_arr: Vec<String> = Vec::new();
 
-    let text = "echo \"hello world\" | type echo | echo hello world2 | echo lmao2";
-    let segments: Vec<&str> = text.split('|').map(str::trim).collect();
+    let text = "echo \"hello world\" | type --f -i echo | echo hello world2 | echo lmao2 > grep \"this words\" > cat ~/Document/new_folder/lmao.txt".to_owned();
+    let re = Regex::new(r#""[^"]*"|[|<>]|\S+"#).unwrap();
 
-    let re = Regex::new(r#""[^"]*"|\S+"#).unwrap();
-
-
-    for s in segments{
-        for arg in re.find_iter(s){
-            arg_arr.push(arg.as_str().to_string());
-        }
+    for s in re.captures_iter(text.as_str()){    
+            arg_arr.push(s.get(0).unwrap().as_str().to_string());
     }
 
     println!("DEBUG (tokenized string from tokenizer): {:?}", arg_arr);
